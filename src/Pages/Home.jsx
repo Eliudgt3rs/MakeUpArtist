fed3840acff04e857c19e3de2b6eab90605a1f97
Eiud Waititu <eliudgachuki130@gmail.com>
2025-06-20 16:55:26 +0300

    I put bold font on the homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,13 +51,13 @@ const HomePage = () => {
             </p>
             <Link
               to="/booking"
-              className="bg-pink-600 text-white px-6 py-3 rounded-4xl shadow-md hover:bg-pink-700 transition-colors inline-block"
+              className="bg-pink-600 text-white px-6 py-3 rounded-4xl shadow-md hover:bg-pink-700 font-semibold transition-colors inline-block"
             >
               Book an Appointment
             </Link>
              <button
               onClick={handleCall}
-              className="bg-pink-600 text-white px-6 py-3 rounded-4xl shadow-md hover:bg-pink-700 transition-colors inline-block"
+              className="bg-pink-600 text-white px-6 py-3 rounded-4xl font-semibold shadow-md hover:bg-pink-700 transition-colors inline-block"
             >
               Call Now
             </button>
