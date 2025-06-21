61f9589743bf6d814a83017309475b6f36482fd8
Eiud Waititu <eliudgachuki130@gmail.com>
2025-06-21 11:11:36 +0300

    Refactor base at viteconfig

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-  //base: process.env.VITE_BASE_PATH || '/MakeUpArtist'
+  base: process.env.VITE_BASE_PATH || '/MakeUpArtist'
 })
